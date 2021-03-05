7af85f818cdfcbb126dbfa3edf15a500e7282504
Noah Zemlin <noah.zemlin@gmail.com>
2021-03-05 16:41:51 -0600

    Change serial_node.py to use CAN instead of Serial/UART

--- firmware/drive/cu.py
+++ firmware/drive/cu.py
@@ -7,7 +7,7 @@ import can
 # bus = can.interface.Bus(bustype='slcan', channel='/dev/ttyACM0', bitrate=100000)
 
 # Stock slcan firmware on Windows
-bus = can.interface.Bus(bustype='slcan', channel='COM3', bitrate=100000)
+bus = can.interface.Bus(bustype='slcan', channel='/dev/igvc-can-835', bitrate=100000)
 
 msg = can.Message(arbitration_id=0xc0ffee,
                   data=[0, 25, 0, 1, 3, 1, 4, 1],
